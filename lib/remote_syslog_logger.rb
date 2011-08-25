8ddc54ca0b50a89ee40d6d078e9d544722570b11
Isaac Kearse <isaac@kearse.co.nz>
2011-08-25 15:16:51 +1200

    Set up remote_syslog_logger to work with heroku

--- lib/remote_syslog_logger.rb
+++ lib/remote_syslog_logger.rb
@@ -1,11 +1,15 @@
 
 require 'remote_syslog_logger/udp_sender'
 require 'logger'
+require 'uri'
 
 module RemoteSyslogLogger
   VERSION = '1.0.3'
 
-  def self.new(remote_hostname, remote_port, options = {})
-    Logger.new(RemoteSyslogLogger::UdpSender.new(remote_hostname, remote_port, options))
+  def self.new(remote_hostname)
+    url = URI.parse remote_hostname
+    logger = Logger.new(RemoteSyslogLogger::UdpSender.new(url.host, url.port, :program => url.path[1..-1]))
+    logger.level = Logger.const_get(([ENV["LOG_LEVEL"].to_s.upcase, "INFO"] & %w[DEBUG INFO WARN ERROR FATAL UNKNOWN]).compact.first)
+    logger
   end
 end
